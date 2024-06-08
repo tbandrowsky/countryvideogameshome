34d6092d57b9442a5dfe4feb3a14244243bbf8f3
tbandrowsky <todd.bandrowsky@gmail.com>
2024-06-08 00:41:39 -0500

    release working rotation

--- src/Background.js
+++ src/Background.js
@@ -1,9 +1,18 @@
 import './App.css';
+import backimage from './images/starbackground.png'
 
 const Background = ({children}) => {
     return(
         <div class="background-bottom">
-            <div class="background-layer1"></div>
+            <div class="background-layer-container">
+                <div class="background-layer1">
+                    <img src={backimage} alt="spinning background, stars" />
+                </div>
+                <div class="background-layer1b">
+                    <img src={backimage} alt="spinning background, stars" />
+                </div>
+            </div>
+            <div class="background-layer3"></div>
             <div class="background-layer2"></div>
             <div class="background-children">{children}</div>
         </div>
