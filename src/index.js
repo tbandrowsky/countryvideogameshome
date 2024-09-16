91482070956a391e44719fe36ec7d7eb17b2d2a1
tbandrowsky <todd.bandrowsky@gmail.com>
2024-09-16 09:35:56 -0500

    Add google tracker

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import Background from './Background';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-   <BrowserRouter>
+    <BrowserRouter>
         <Background>
             <Caption/>
             <Routes>
