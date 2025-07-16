b468451d309ea84aaaa34a920c66027db9b08537
Todd Bandrowsky <todd.bandrowsky@gmail.com>
2025-07-15 20:55:01 -0500

    Modify that chumpy to be cool.,

--- src/Caption.js
+++ src/Caption.js
@@ -3,7 +3,8 @@ import './Caption.css';
 function Caption() {
   return (
     <div class="Caption">
-      <div class="countrytitle1">COUNTRY VIDEO GAMES</div>
+          <div class="countrytitle1">COUNTRY</div>
+          <div class="countrytitle1">VIDEOGAMES</div>         
     </div>
   );
 }
