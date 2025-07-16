002b60e9a37634ac14f28d2e86cdb9943ee28747
tbandrowsky <todd.bandrowsky@gmail.com>
2025-07-16 08:32:34 -0500

    Let's see if this works.

--- src/News.js
+++ src/News.js
@@ -4,18 +4,29 @@ import colorpartystore from './images/colorpartymsstore.png'
 function News() {
   return (
       <div class="contentbackground">
-            <h1>Welcome to Country Video Games</h1>
-            <h2>All our applications are $1, or close to it.</h2>
+          <h1>MEANWHILE</h1>
+          <i>AT THE SECRET COUNTRY VIDEOGAMES COMPOUND</i>
+          <h2>WEB SITE</h2>
+          <h3>REPO</h3>
+          <a href="https://github.com/tbandrowsky/countryvideogameshome">Website Repo</a>
+          <h3>UPDATE</h3>
+          <p>We're working on our web site to be more friendly and modern.  Our vision is as CountryVideoGames as sort of a flea market
+              rural or hood feel.  Expanding out pages, cleaning up the navigation is a focus.  We're going to use Corona to
+              add collaboration features and demonstrations to showcase our power, and have microgames throughout.</p>
 
-            <h2>Corona App Server - Free</h2>
-            <p>Create web apis instantly from a class definition file.</p>
-            <p>Download zip, extract to folder, and either run from the terminal or invoke the batch.</p>
+          <h2>CORONA API SERVER</h2>
+          <h3>REPO</h3>
+          <a href="https://github.com/tbandrowsky/countrybit">Corona Repo</a>
+          <h3>UPDATE</h3>
+          <p>Create web apis and services instantly from a class definition file.</p>
             <p>
                 <a href="coronaserver.zip">Download Corona Preview (11/28/2024)</a>
             </p>
 
-            <h2>Color Party - $1</h2>
-            <p>Color Party.  Color Party lets you make light shows out of anything that moves instantly, with one or more web cams attacked to your Windows laptop or desktop.</p>
+            <h2>Color Party</h2>
+              <h3>REPO</h3>
+              <a href="https://github.com/tbandrowsky/countrybit">Corona Repo (includes color party)</a>
+            <p>Color Party.  We haven't done anything here in a bit, but it is a fun application.</p>
             <p>
                 <a href="ms-windows-store://pdp/?productid=9PP1DQXGWG0N"><img src={colorpartystore} width="500" alt="Color Party in Windows Store" /></a>
             </p>
