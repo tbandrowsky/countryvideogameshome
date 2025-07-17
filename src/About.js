ccc33d66b09be4978dad2909742d383f8dab936b
Todd Bandrowsky <todd.bandrowsky@gmail.com>
2025-07-17 13:35:21 -0500

    Update the policy pages... part of cleaning up ui.

--- src/About.js
+++ src/About.js
@@ -3,11 +3,11 @@ function About() {
   return (
       <div class="contentbackground">
       <h1>
-      About
+              ABOUT
       </h1>
-        <h2>Writers</h2>
-          <h3>Todd Bandrowsky</h3>
-          <p>Todd is the founder of Country Video Games.  He writes applications and services in C++, C#, JavaScript, short stories and poetry.  He's also rumored to have
+        <h2>WRITERS</h2>
+          <h3 style={{ marginTop: "12px" }}>Todd Bandrowsky</h3>
+          <p style={{ marginTop: "0px" }}>Todd is the founder of Country Video Games.  He writes applications and services in C++, C#, JavaScript, short stories and poetry.  He's also rumored to have
 learned a few chords on his guitar.</p>
         </div>
   );
--- src/Privacy.js
+++ src/Privacy.js
@@ -2,27 +2,15 @@
 function Privacy() {
   return (
       <div class="contentbackground">
-      <h1>Country Video Games PRIVACY POLICY</h1>
-      <h2>
-      Country Video Games is interested in your money, not your data.
-      </h2>
+      <h1>PRIVACY POLICY</h1>
       <p>
-      Now, we don't want your money so bad that we're going to ship you some piece of doodoo spyware. 
-      We don't want your money so bad that we'd t mine your stuff to 
-      send it off to some AI sales guy.  
-      We don't want it so bad we'll start showing bulldoodoo links 
-      to other people's stuff and maybe worst of all to more piece of doodoo spyware.  
-      </p>
-      <p>
-      We just don't collect your data and we don't ship spy stuff to collect it.
-      </p>
-      <p>
-      Right now, our product is just a fun game, and we'd appreciate it if you could be 
-      so kind as to give us a go.  We just want you to be entertained, with no strings attached.
-      </p>
-      <p>
-      Thank you in advance for your kind consideration,
-      </p>
+              Country Video Games respects your privacy and is committed to protecting it. This Privacy Policy explains how we collect, use, and disclose information about you when you visit our website or use our services.
+          </p>
+          <i>County Video Games.</i>
+          <ul>
+              <li>Does Not Collect your personal information to sell or donate.</li>
+              <li>Uses modern accuracy assurance practices to guard against breaches.</li>
+          </ul>
       <p class="signature">
       Todd Bandrowsky
       </p>
