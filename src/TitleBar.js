f7852b5364b1a2253db2f1db894353eaac1eb1dd
tbandrowsky <todd.bandrowsky@gmail.com>
2025-07-17 19:22:52 -0500

    workin

--- src/TitleBar.js
+++ src/TitleBar.js
@@ -3,7 +3,6 @@ import './index.css';
 
 function TitleBar(props) {
     const { title } = props;
-    console.log({ "Title" : title, "Props":props });
     return (
         <div className="titlearea">{ title }</div>
     );
