deba2f843bc9f7218d3ab6579caa7a5909db6ee8
Todd Bandrowsky <todd.bandrowsky@gmail.com>
2025-07-27 00:56:45 -0500

    edit field

--- src/Corona/TextEditField.js
+++ src/Corona/TextEditField.js
@@ -7,34 +7,35 @@ import InputMask from 'react-input-mask';
 export default function TextEditField(props)
 {
 
-    if ('input_mask' in props)
-    {
+    if ('enum' in props) {
+
+    }
+    else if ('input_mask' in props) {
         return <InputMask mask={props.input_mask} placeholder={props.placeholder} />
     }
-
-    let format = '';
-    if ('format' in props) {
-
-    }
-    if ('max_length' in props) {
-
-    }
-    if ('min_length' in props) {
-
-    }
-    
-    if ('input_mask' in props || 'placeholder' in props)
+    else
     {
-        return <InputMask mask={props.input_mask} placeholder={props.placeholder} />
-    }
+        let format = '';
+        let max_length = 0;
+        let min_length = 0;
+        let match_pattern = '';
+        let enum_values = [];
+        let placeholder = '';
 
-    else if ()
-    {
-        return <input type="text" maxLength={maxLength}></input>
+        if ('format' in props) {
+            format = props.format;
+        }
+        if ('max_length' in props) {
+            max_length = props.max_length;
+        }
+        if ('min_length' in props) {
+            min_length = props.min_length;
+        }
+        if ('match_pattern' in props) {
+            match_pattern = props.match_pattern;
+        }
+        if ('placeholder' in props) {
+            placeholder = props.placeholder;
+        }
     }
-    else if ()
-    {
-        return <input type="text" maxLength={maxLength}></input>
-    }
-
 }
