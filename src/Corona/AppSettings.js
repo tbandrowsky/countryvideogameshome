59ff09eedfb8392e9486d79520a9cb9021c26ac1
Todd Bandrowsky <todd.bandrowsky@gmail.com>
2025-09-18 22:11:08 -0500

    local dev

--- src/Corona/AppSettings.js
+++ src/Corona/AppSettings.js
@@ -3,6 +3,6 @@ export const AppSettings = {
     TokenKey: "CoronaToken",    
     ApplicationName: "Revolution Demonstration",
     GetBaseUrl: function () {
-        return "https://www.mightyware.com/revolution";
+        return "https://localhost:5678/revolution";
     }
 }
