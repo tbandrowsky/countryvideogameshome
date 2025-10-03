f7607cffa027424f4f14dfc8f025cf2a4d9a09ec
tbandrowsky <todd.bandrowsky@gmail.com>
2025-10-03 13:07:02 -0500

    Closing in on getting object search to work

--- src/Revolution/CreateUserForm.js
+++ src/Revolution/CreateUserForm.js
@@ -10,7 +10,6 @@ import { useNavigate } from "react-router";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAtom } from '@fortawesome/free-solid-svg-icons';
 import { faSquareCaretLeft } from '@fortawesome/free-solid-svg-icons';
-
 export default function CreateUserForm(props) {
 
     const [request, setRequest] = useState({ });
@@ -26,7 +25,7 @@ export default function CreateUserForm(props) {
             gridTemplateRows: "auto auto auto auto auto auto"
         },
         body_fields: [
-            { json_field_name: "user_name", column: '1/4', row: 1, field_type: "string", format: "email", placeholder: "E-Mail", max_length: 100, min_length: 4, autocomplete: 'email' },
+                { json_field_name: "user_name", column: '1/4', row: 1, field_type: "string", format: "email", placeholder: "E-Mail", max_length: 100, min_length: 4, autocomplete: 'email' },
             { json_field_name: "password1", column: 1, row: 2, field_type: "string", format: "password", placeholder: "Password 1", max_length: 50, min_length: 8, autocomplete: 'new-password' },
             { json_field_name: "password2", column: 2, row: 2, field_type: "string", format: "password", placeholder: "Password 2", max_length: 50, min_length: 8, autocomplete: 'new-password' },
             { json_field_name: "first_name", column: 1, row: 3, field_type: "string", format: "name", placeholder: "First Name", max_length: 100, min_length: 1, autocomplete: 'given_name' },
@@ -36,7 +35,7 @@ export default function CreateUserForm(props) {
             { json_field_name: "city", column: 1, row: 5, field_type: "string", format: "text", placeholder: "City", max_length: 50, min_length: 8, autocomplete: 'address-level2' },
             { json_field_name: "state", column: 2, row: 5, field_type: "string", format: "text", placeholder: "State", max_length: 20, min_length: 2, autocomplete: 'address-level1' },
             { json_field_name: "zip", column: 3, row: 5, field_type: "string", format: "text", placeholder: "Zip", max_length: 10, min_length: 5, autocomplete: 'postal-code' }
-        ],
+    ],
         put_value
     };
 
@@ -57,7 +56,13 @@ export default function CreateUserForm(props) {
                             redoMessage: 'Unable to process application.'
                         });
                         setError({ success: response.success, message: response.message, inProgress: false, errors:response.errors });
-                        nav(response.form, { ...response.form_props });
+                        if (response.success) {
+                            nav_state = { ...response };
+                        } else {
+                            nav_state = { ...props};
+                        }
+
+                        nav(response.form, { state: nav_state });
                     }
                 }><FontAwesomeIcon icon={faAtom} />ENLIST</button>
                 <button id="cancelButton" disabled={error.inProgress} onClick={
@@ -65,6 +70,7 @@ export default function CreateUserForm(props) {
                         nav('/Revolution/Login');
                     }
                 }><FontAwesomeIcon icon={faSquareCaretLeft} />CANCEL</button>
+
             </div>
         </div>
     );
